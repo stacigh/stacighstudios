a881bc33cd03c04d5bbddc9963395f1edcd577f6
Stacey Lane <hello@stacigh.com>
2014-05-12 22:38:45 -0700

    Layout tweaks

---
+++ js/min/scripts-ck.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(e){var t=e(window).height();e(".site-header, .content-area").css("height",t),e(window).resize(function(){var t=e(window).height();e(".site-header, .content-area").css("height",t)})});
--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,19 @@ jQuery(document).ready(function($) {
     // and other libraries also using $ will not be accessible under this shortcut
     // Don't want this behavior? Delete the dollar sign from the function.
     
+	//GET BROWSER WINDOW HEIGHT
+	var currHeight = $(window).height();
+	//SET HEIGHT OF SIDEBAR AND CONTENT ELEMENTS
+	$('.site-header, .content-area').css('height', currHeight);
+
+	//ON RESIZE OF WINDOW
+	$(window).resize(function() {
+
+		//GET NEW HEIGHT
+		var currHeight = $(window).height();	
+		//RESIZE BOTH ELEMENTS TO NEW HEIGHT
+		$('.site-header, .content-area').css('height', currHeight);
+
+	});
 
 });
